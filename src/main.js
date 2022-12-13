bdcca38a5867c3eb4cd711fe060a6618856fbc6e
Ian <fancyian05180910@gmail.com>
2022-12-13 10:40:30 +0800

    新增 bootstrap 套件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Bootstrap from 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
+Vue.use(Bootstrap)
 
 new Vue({
   render: h => h(App),
