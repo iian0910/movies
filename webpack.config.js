32f2313d89a16711538e15f76408ab92d3fbb40f
Ian <fancyian05180910@gmail.com>
2022-12-14 20:33:35 +0800

    調整 webpack 檔案

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
+const IN_PRODUCTION = process.env.NODE_ENV === "production";
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            "style-loader",
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
-          ],
-        },
-      ],
-    },
-  };
+  publicPath: IN_PRODUCTION ? '/movies/' : '/',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
